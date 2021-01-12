7aa769733d883b8edaed5ebcaf71ee0418cedb9f
Richman Clifford <richmanlclifford@eziteksolutions.com>
2021-01-12 20:10:17 +0000

    Added validation for mbti records creation requests

---
+++ api/app/Http/Requests/ApiRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Foundation\Http\FormRequest as LaravelFormRequest;
+
+abstract class ApiRequest extends LaravelFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract public function rules();
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    abstract public function authorize();
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(response()->json([
+            'errors' => $errors
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+    /**
+     * Handle a failed authorization attempt.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    protected function failedAuthorization()
+    {
+        $errors = ['You are not authorized to perform this function'];
+
+        throw new HttpResponseException(response()->json([
+            'errors' => $errors
+        ], JsonResponse::HTTP_FORBIDDEN));
+    }
+}
---
+++ api/app/Http/Requests/MBTI/CreateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\MBTI;
+
+use App\Models\Question;
+use App\Http\Requests\ApiRequest;
+
+class CreateRequest extends ApiRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'responses' => 'required|min:' . Question::count() . '|max:' . Question::count(),
+            'responses.*.question_id' => 'required|exists:questions,id',
+            'responses.*.choice' => 'required|numeric|min:1|max:7',
+            'responses.*.dimension' => 'required|in:EI,SN,TF,JP',
+            'responses.*.direction' => 'required|in:1,-1'
+        ];
+    }
+}
